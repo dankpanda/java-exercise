178b86e3e0dc0b638f777c2f6b9ecd8b75544908
dankpanda <yowenyo17@gmail.com>
2020-03-05 15:11:05 +0700

    updating number 8

--- java_array_exercise/src/java_array_exercise/Eight.java
+++ java_array_exercise/src/java_array_exercise/Eight.java
@@ -1,9 +1,54 @@
 package java_array_exercise;
 
-public class Eight {
+import java.util.ArrayList;
 
+
+public class Eight {
+	private static void max_split(int arr[])
+	{
+		int max_counter = 0;
+		int max = 0;
+		int second_max=0;
+		int temp[] = arr;
+		
+		for(int i = 0; i <arr.length;i++)
+		{
+			if(arr[i] > max) max = arr[i];
+			if(arr[i] > second_max & arr[i] != max) second_max = arr[i];
+		}
+		
+		
+		for(int i = 0; i <arr.length;i++)
+		{
+			if(arr[i] == max) max_counter++;
+		}
+		
+		ArrayList<Integer> arr1 = new ArrayList<Integer>(arr.length+max_counter);
+		
+		for(int i = 0;i <arr.length;i++)
+		{
+			if(arr[i] != max) arr1.add(temp[i]);
+			
+			else
+			{
+				arr1.add(second_max);
+				arr1.add(max-second_max);
+			}
+			
+		}
+		System.out.print(arr1.toString());
+		
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int my_array[] = {4,8,3,6,2,9,9};
+		System.out.print("Sample input >> ");
+		for(int i = 0; i < my_array.length;i++ )
+		{
+			System.out.print(my_array[i]+" ");
+		}
+		
+		System.out.print("\nOutput >> ");
+		max_split(my_array);
 
 	}
 
